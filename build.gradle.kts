17abb1c9c55026d42f907ffa5832e530aff1a470
Andres Felipe Morales Anaya <andres.nmorales@mercadolibre.com.co>
2024-09-12 22:23:12 -0500

    Subtema 3-4-6 Uso de nomenclaturas, cohesion, acoplamiento, anotadores y genericos

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,16 @@ repositories {
 }
 
 dependencies {
+    // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-databind
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.0-rc1")
+
+    // https://mvnrepository.com/artifact/org.projectlombok/lombok
+    compileOnly("org.projectlombok:lombok:0.11.0")
+    annotationProcessor("org.projectlombok:lombok:1.18.34")
+
+    testCompileOnly("org.projectlombok:lombok:1.18.34")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.34")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
---
+++ src/main/java/org/example/topics/cohesion/LeerArchivo.java
@@ -0,0 +1,22 @@
+package org.example.topics.cohesion;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+
+import java.io.File;
+import java.util.List;
+
+public class LeerArchivo {
+  // Leer el archivo JSON
+  private final ObjectMapper mapper;
+
+  public LeerArchivo() {
+    this.mapper = new ObjectMapper();
+  }
+
+  @SneakyThrows
+  public List getList(String fileName) {
+    String file = ProcesadorNumeros.class.getClassLoader().getResource(fileName).getFile();
+    return mapper.readValue(new File(file), List.class);
+  }
+}
